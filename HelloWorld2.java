198c1ce472590ac4f8a13f1c4b1c54e606ed977c
suma23 <52690481+suma23@users.noreply.github.com>
2019-07-11 09:14:14 -0400

    Create HelloWorld2.java

---
+++ HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2{
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, Worldbranch2");
+    }
+
+}
