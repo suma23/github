fd2eacab208a663748f91e8b6024423655fc9c09
suma23 <52690481+suma23@users.noreply.github.com>
2019-07-15 02:14:49 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, thisisWorldbranch1 automated build for 15th july");
+        System.out.println("Hello, thisisWorldbranch1 automated build for 15th july latest one");
     }
 
 }
