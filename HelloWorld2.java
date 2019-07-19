c45891235725b6d2dbdf5631b0cb5990fef96a4d
suma23 <52690481+suma23@users.noreply.github.com>
2019-07-19 03:28:30 -0400

    Update HelloWorld2.java

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -2,7 +2,7 @@ public class HelloWorld2{
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, Worldbranch2 build automated");
+        System.out.println("Hello, Worldbranch2 build automated branch");
     }
 
 }
